7de799f98b8c7d01a21c512e268912ee373f856f
Ananda Aditya Putra <anandaadityaputra11@gmail.com>
2022-04-22 20:00:01 +0700

    memperbaiki kesalahan URI

---
+++ src/lib/config.js
@@ -0,0 +1,17 @@
+const getHost = () => {
+  const protocol = window.location.protocol;
+  const host = window.location.host;
+
+  return `${protocol}//${host}`;
+};
+
+
+
+const config = {
+  SPOTIFY_BASE_URL: 'https://api.spotify.com/v1',
+  SPOTIFY_SCOPE: 'playlist-modify-private',
+  HOST: getHost(),
+};
+
+export default config;
+
